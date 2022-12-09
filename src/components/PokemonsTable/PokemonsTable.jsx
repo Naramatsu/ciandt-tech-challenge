a001b7c3b3141155129cda5cb42a2e9d2abe63c2
Narmatsu <joncyy_19@hotmail.com>
2022-12-09 17:36:39 -0500

    added pokemon table component to show stats

---
+++ src/components/PokemonsTable/PokemonsTable.jsx
@@ -0,0 +1,75 @@
+import React, { useMemo, useState, useEffect } from "react";
+import { Button } from "@mui/material";
+import { AgGridReact } from "ag-grid-react";
+import "./PokemonsTable.style.scss";
+import { useContext } from "react";
+import { AppContext } from "../../context";
+
+const PokemonsTable = ({ onHide }) => {
+  const { fight } = useContext(AppContext);
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "500px", width: "100%" }), []);
+  const [rowData, setRowData] = useState([]);
+  const [columnDefs] = useState([
+    { field: "pokemon" },
+    { field: "hp" },
+    { field: "weight" },
+    { field: "height" },
+    { field: "attack" },
+    { field: "defense" },
+    { field: "specialAttack" },
+    { field: "specialDefense" },
+    { field: "speed" },
+  ]);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      sortable: true,
+      filter: true,
+      resizable: true,
+    };
+  }, []);
+
+  useEffect(() => {
+    setRowData(
+      fight.map((pokemon) => {
+        const [hp, attack, defense, specialAttack, specialDefense, speed] =
+          pokemon.stats;
+        return {
+          pokemon: pokemon.name,
+          hp: hp.base_stat,
+          weight: pokemon.weight,
+          height: pokemon.height,
+          attack: attack.base_stat,
+          defense: defense.base_stat,
+          specialAttack: specialAttack.base_stat,
+          specialDefense: specialDefense.base_stat,
+          speed: speed.base_stat,
+        };
+      })
+    );
+  }, [fight]);
+
+  return (
+    <section className="pokemons_stats">
+      <h3>Stats</h3>
+      <div style={containerStyle}>
+        <div style={gridStyle} className="ag-theme-alpine">
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs}
+            rowSelection={"multiple"}
+            defaultColDef={defaultColDef}
+            sideBar={"columns"}
+          ></AgGridReact>
+        </div>
+      </div>
+      <Button variant="outlined" onClick={onHide}>
+        Close
+      </Button>
+    </section>
+  );
+};
+
+export default PokemonsTable;
