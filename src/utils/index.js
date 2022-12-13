7447a1702615de385d9ebf96dd1fca33b939a2a8
Narmatsu <joncyy_19@hotmail.com>
2022-12-13 14:25:40 -0500

    removed localstorage functions in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,3 @@
-export const saveItemInLocalStorage = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-export const getItemInLocalStorage = (key) => {
-  return JSON.parse(localStorage.getItem(key));
-};
-
 export const removeDuplicateItems = (items) => {
   const hash = {};
   return items.filter((current) => {
